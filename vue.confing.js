5e939b8bb2631458ce6addd8d71bfd74254f085f
chiakilalala <chiaki205102@gmail.com>
2020-10-13 14:27:14 +0800

    config

--- vue.confing.js
+++ vue.confing.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ?
-        '/hotel_vue/' : '/',
+        '/Vue_Hotel/' : '/',
     build: {
         assetsPublicPath: ''
     }
